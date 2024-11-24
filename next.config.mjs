ae752f8958a92dfe6d189e54508fb2d6cb309399
yakkshit Reddy <saiyakkshit2001@gmail.com>
2024-11-24 20:12:42 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,62 @@
+import { withContentCollections } from "@content-collections/next";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: process.env.NODE_ENV === "production" ? "standalone" : undefined,
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    optimizeCss: true,
+  },
+  images: {
+    domains: ["localhost", "cdn.magicui.design"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/X4BBMBjHNf",
+        permanent: true,
+      },
+      {
+        source: "/components",
+        destination: "/docs/components/marquee",
+        permanent: true,
+      },
+      {
+        source: "/components/:path*",
+        destination: "/docs/components/:path*",
+        permanent: true,
+      },
+      {
+        source: "/docs/components",
+        destination: "/docs/components/marquee",
+        permanent: true,
+      },
+      {
+        source: "/r",
+        destination: "/r/index.json",
+        permanent: true,
+      },
+      {
+        source: "/r/index",
+        destination: "/r/index.json",
+        permanent: true,
+      },
+      {
+        source: "/r/:name((?!index\\.json|styles/).*)",
+        destination: "/r/styles/default/:name.json",
+        permanent: true,
+        missing: [
+          {
+            type: "query",
+            key: "_redirected",
+            value: undefined,
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default withContentCollections(nextConfig);
